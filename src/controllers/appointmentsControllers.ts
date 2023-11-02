0aadf198a085b892d1e8b6e73d59194ea970848e
AndrésLabat <andreslabat89@gmail.com>
2023-11-02 18:45:01 +0100

    refactor: delete query with .find in getAppointmentDetail beacause is useless when I use the relationship in Appointment table with user table

--- src/controllers/appointmentsControllers.ts
+++ src/controllers/appointmentsControllers.ts
@@ -506,21 +506,18 @@ const getAppointmentDetail = async (req: Request, res: Response) => {
             where: {
                 client_id: id
             },
-            relations: ["appointmentPortfolios"]
+            relations: ["appointmentPortfolios", "worker"]
         })
 
         const appointmentsUserForShow = await Promise.all(appointmentsUser.map(async (obj) => {
-            const { status, worker_id, client_id, appointmentPortfolios, ...rest } = obj;
+            const { status, worker_id, client_id, appointmentPortfolios, worker, ...rest } = obj;
             const nameProduct = obj.appointmentPortfolios.map((obj) => obj.name)
+            const infoWorker = obj.worker
 
-            const worker = await User.findOneBy({
-                id: worker_id
-            });
-
-            if (worker) {
-                const full_name = worker.full_name
-                const email = worker.email;
-                const is_active = worker.is_active;
+            if (infoWorker) {
+                const full_name = infoWorker.full_name
+                const email = infoWorker.email;
+                const is_active = infoWorker.is_active;
                 const name = nameProduct[0]
                 return { full_name, email, name, is_active, ...rest };
             }
